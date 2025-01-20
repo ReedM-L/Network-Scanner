f5f02249f215c3876d8aa067a0f91e7eb939a4df
Reed Lyle <lylereedm@gmail.com>
2025-01-19 20:17:07 -0500

    more practice with scapy, learned how to send ICMP requests, tested by sending to 8.8.8.8, want to set up functionality to scan a port and report whether it is open, closed, or filtered. Still need to figure out how to do a ping sweep and how to combine multiple elements to make a tool similar to Nmap.

--- networkScanner.py
+++ networkScanner.py
@@ -1,9 +1,13 @@
 from scapy.all import *
-from scapy.all import IP, ICMP
+from scapy.all import IP, ICMP, TCP
 
 # ICMP Ping
-packet = IP(dst = "192.168.1.0/30")
-ans, unans = sr(packet/ICMP(), timeout=3)
+
+packet = IP(dst = "8.8.8.8")
+ans, unans = sr(packet/ICMP()/TCP(dport=80,flags="S"), timeout=3)
+
+if (ans):
+    response = sr1(packet/TCP(dport=80, flags="S"))
 
 # Sending packet
 
