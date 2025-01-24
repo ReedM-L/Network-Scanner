32fc37b7a5c531d92c67e15c69df0613ae22c355
Reed Lyle <lylereedm@gmail.com>
2025-01-24 12:37:03 -0500

    Fixed logic errors, started scanning localhost, ICMP request working however, after port scanning want to direct output to a seperate text file. Having some issues with that as I need to learn more about scapy such as how the .summary() method can be utilized, once I do that I'll have to learn how to understand and read the send-recieve list better and from there determine if certain ports are open, closed, or filtered.

--- networkScanner.py
+++ networkScanner.py
@@ -1,7 +1,8 @@
 from scapy.all import *
 from scapy.all import IP, ICMP, TCP
+import sys
 
-# ICMP Ping
+
 def ipValidation(ip_address):
     parts = ip_address.split(".")
     if len(parts) != 4:
@@ -10,32 +11,37 @@ def ipValidation(ip_address):
         if not part.isdigit():
             return False
         numVal = int(part)
-        if part < 0 or part > 255:
+        if numVal < 0 or numVal > 255:
             return False
+    return True
 
 
 def Ping(ip_address):
-    packet = IP(dst = ip_address)
-    ans, unans = sr(packet/ICMP(), timeout=3)
-    if ans:
-        print(ans)
-        print("Scanning ports: 0-1000")
-        port = range(0,1000)
-        while(port in port):
-            response = sr1(packet/TCP(dport = port, flags="S"))
-            print(response)
-    else:
-        print(unans)
+    with open("results.txt", "w") as f:
+        packet = IP(dst = ip_address)
+        ans, unans = sr(packet/ICMP(), timeout=3)
+        if ans:
+            f.write(ans)
+            print("Scanning ports: 0-1000")
+            port = range(0,1000)
+            for current_port in port:
+                response = sr(packet/TCP(dport = current_port, flags="S"))
+                f.write(str(current_port)+ ": " + str(response))
+        else:
+            print(unans)
+    
 
         
 def main():
     ip_address = input("Enter the IP address to scan: ")
     if ipValidation(ip_address):
-        confimation = input("Continue with ping of network? Y/N")
-        if confimation == "Y" or "y":
+        confirmation = input("Continue with scan of network? Y/N: ")
+        if confirmation == "Y" or confirmation == "y":
             Ping(ip_address)
         else:
             sys.exit()
+    else:
+        print("Invalid IP address")
 
 
 if __name__ == "__main__":
